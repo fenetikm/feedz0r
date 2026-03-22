493733490afbc079a4ee05626ea47a7d322f59e6
Michael Welford <mail@michaelwelford.com>
2026-03-22 20:13:00 +1030

    chore: tidy up config.

--- internal/config/config.go
+++ internal/config/config.go
@@ -11,7 +11,6 @@ const configFileName = "config.yml"
 type FetchConfig struct {
 	Timeout     int `yaml:"timeout"`
 	RefreshMins int `yaml:"refresh"`
-	MaxFeeds    int `yaml:"maxfeeds"`
 }
 
 type Config struct {
